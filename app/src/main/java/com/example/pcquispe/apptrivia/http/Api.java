8766bc81d84837835bb79529f008ce2f308971d4
kevinQuispe <kquispe07@hotmail.com>
2018-02-22 16:42:47 -0500

    Initial commit

---
+++ app/src/main/java/com/example/pcquispe/apptrivia/http/Api.java
@@ -0,0 +1,8 @@
+package com.example.pcquispe.apptrivia.http;
+
+/**
+ * Created by PCQUISPE on 2/22/2018.
+ */
+
+public class Api {
+}
---
+++ app/src/main/java/com/example/pcquispe/apptrivia/util/Config.java
@@ -0,0 +1,9 @@
+package com.example.pcquispe.apptrivia.util;
+
+/**
+ * Created by PCQUISPE on 2/22/2018.
+ */
+
+public class Config {
+    static String URL_SERVER="http://192.168.1.1";
+}
